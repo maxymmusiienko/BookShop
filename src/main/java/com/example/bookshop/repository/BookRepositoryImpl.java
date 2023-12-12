911e265b1f3792e968e8dbd581bb1ed41a65448f
Maksym Musiienko <maxymvm@gmail.com>
2023-12-12 12:42:37 +0200

    added Dto models, BookController, missing methods in repository and service

--- src/main/java/com/example/bookshop/repository/BookRepositoryImpl.java
+++ src/main/java/com/example/bookshop/repository/BookRepositoryImpl.java
@@ -3,7 +3,6 @@ package com.example.bookshop.repository;
 import com.example.bookshop.model.Book;
 import java.util.List;
 import java.util.Optional;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
--- src/main/java/com/example/bookshop/service/BookService.java
+++ src/main/java/com/example/bookshop/service/BookService.java
@@ -3,7 +3,6 @@ package com.example.bookshop.service;
 import com.example.bookshop.dto.BookDto;
 import com.example.bookshop.dto.CreateBookRequestDto;
 import java.util.List;
-import java.util.Optional;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto book);
